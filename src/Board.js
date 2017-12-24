5b5882459ca92963f02060134d529c2b63cea7d6
tom1700 <tomekm1700@gmail.com>
2017-12-24 10:55:31 +0100

    Adding overlay

--- src/Board.js
+++ src/Board.js
@@ -3,13 +3,50 @@ const config = require('./config');
 const Tile = require('./Tile');
 
 const template = `
-<div class="board">
-    <Tile v-bind:width="100" />
+<div class="board" ref="board">
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    <Tile :height="tileHeight" :flex="tileFlexBasis" />
+    
 </div>
 `;
 
 const Board = Vue.component('Board', {
     props: ['score'],
     template: template,
-
+    mounted: function() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.getBoardWidth);
+            this.getBoardWidth()
+        });
+    },
+    data: function() {
+        return {
+            boardWidth: 0,
+        };
+    },
+    methods: {
+        getBoardWidth: function() {
+            this.boardWidth = this.$refs.board.clientWidth;
+        }
+    },
+    computed: {
+        tileHeight: function() {
+            return Math.floor(this.boardWidth / config.horizontalAmount);
+        },
+        tileFlexBasis: function() {
+            return Math.floor(100 / config.horizontalAmount);
+        }
+    }
 });
--- src/Tile.js
+++ src/Tile.js
@@ -4,20 +4,19 @@ const template = `
 <div
     class="tile"
     v-bind:style="tileSize"
-    ref="tile"
 >
-Tile
+    <div class="content overlayed">Tile</div>
 </div>
 `;
 
 const Tile = Vue.component('Tile', {
-    props: ['width'],
+    props: ['height', 'flex'],
     template: template,
     computed: {
         tileSize: function() {
             return {
-                width: `${this.width}px`,
-                height: `${this.width}px`
+                ['flex-basis']: `${this.flex}%`,
+                height: `${this.height}px`
             }
         }
     }
