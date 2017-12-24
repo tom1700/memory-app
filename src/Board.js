c162b822f07b5eb33246c8ae9f53d736dbbd4b89
tom1700 <tomekm1700@gmail.com>
2017-12-24 13:39:17 +0100

    Adding data to tiles

--- src/Board.js
+++ src/Board.js
@@ -5,6 +5,7 @@ const template = `
 <div class="board" ref="board">
     <Tile
         v-for="tile in tiles"
+        :tile="tile"
         :key="tile.key"
         :height="tileHeight"
         :flex="tileFlexBasis"
--- src/Tile.js
+++ src/Tile.js
@@ -1,17 +1,29 @@
 const Vue = require('vue/dist/vue.js');
+const constants = require('./constants');
 
 const template = `
 <div
     class="tile"
     v-bind:style="tileSize"
 >
-    <div class="content overlayed">Tile</div>
+    <div class="content">
+        <div class="sentence"v-if="tile.type === tileType.SENTENCE">
+            {{tile.name}}
+        </div>
+        <div v-if="tile.type === tileType.IMAGE" >
+            {{tile.name}} image
+        </div>
+    </div>
+</div>
 </div>
 `;
 
 const Tile = Vue.component('Tile', {
-    props: ['height', 'flex'],
+    props: ['height', 'flex', 'tile'],
     template: template,
+    data: () => ({
+        tileType: constants.tileType
+    }),
     computed: {
         tileSize: function() {
             return {
