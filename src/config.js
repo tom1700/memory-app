57708e773740cd0c021749dcd4b3e718fb8fd085
tom1700 <tomekm1700@gmail.com>
2017-12-25 18:59:01 +0100

    Enabling multiple lessons

--- src/config.js
+++ src/config.js
@@ -1,37 +1,44 @@
-define({
-    lessonName: 'Body parts',
-    horizontalAmount: 4,
-    subFolder: 'example',
-    tiles: [
-        {
-            name: 'eye',
-            imageSrc: 'eye.png',
-            audioSrc: 'ear.mp3',
-        },
-        {
-            name: 'ear',
-            imageSrc: 'ear.png',
-            audioSrc: 'ear.mp3',
-        },
-        {
-            name: 'hand',
-            imageSrc: 'hand.png',
-            audioSrc: 'ear.mp3',
-        },
-        {
-            name: 'nose',
-            imageSrc: 'nose.png',
-            audioSrc: 'ear.mp3',
-        },
-        {
-            name: 'shoulder',
-            imageSrc: 'shoulder.gif',
-            audioSrc: 'ear.mp3',
-        },
-        {
-            name: 'knee',
-            imageSrc: 'knee.png',
-            audioSrc: 'ear.mp3',
-        },
-    ]
+define(function () {
+    const config = {
+        active: 'bodyParts',
+        bodyParts: {
+            lessonName: 'Body parts',
+            horizontalAmount: 4,
+            subFolder: 'example',
+            tiles: [
+                {
+                    name: 'eye',
+                    imageSrc: 'eye.png',
+                    audioSrc: 'ear.mp3',
+                },
+                {
+                    name: 'ear',
+                    imageSrc: 'ear.png',
+                    audioSrc: 'ear.mp3',
+                },
+                {
+                    name: 'hand',
+                    imageSrc: 'hand.png',
+                    audioSrc: 'ear.mp3',
+                },
+                {
+                    name: 'nose',
+                    imageSrc: 'nose.png',
+                    audioSrc: 'ear.mp3',
+                },
+                {
+                    name: 'shoulder',
+                    imageSrc: 'shoulder.gif',
+                    audioSrc: 'ear.mp3',
+                },
+                {
+                    name: 'knee',
+                    imageSrc: 'knee.png',
+                    audioSrc: 'ear.mp3',
+                },
+            ]
+        }
+    };
+
+    return config[config.active];
 });
